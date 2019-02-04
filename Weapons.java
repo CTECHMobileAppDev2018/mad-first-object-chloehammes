eda598d3be072cb350bef2e496a907238213d694
chloehammes <47236158+chloehammes@users.noreply.github.com>
2019-02-04 09:04:21 -0600

    Update Weapons.java

--- Weapons.java
+++ Weapons.java
@@ -1,1 +1,33 @@
+public class Weapons {
+  
+  private String weaponName;
+  private int weaponDamage;
+  private int weaponAmmo;
+  
+  //Automatically set new weapons with 35 bullets
+  Weapons() {
+    this.weaponAmmo = 35;
+  }
+  
+  //Set and get the weapon name
+  public void setName(String weaponName) { 
 
+    // Making sure the name is always longer than zero
+    if (weaponName.length() < 1) {
+      this.name = "invalid";
+      return;
+    }
+    
+    this.name = weaponName;
+  }
+  
+  // this just gets the name 
+  public String getName() {
+    return this.name;
+  }
+  
+  //Set and get the weapon damage
+  public int setDamage(int weaponDamage) {
+    
+    
+    
