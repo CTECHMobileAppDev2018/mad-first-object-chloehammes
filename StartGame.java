25649f017e054c90502d5d6f19a5fdbdcd4438a6
chloehammes <47236158+chloehammes@users.noreply.github.com>
2019-02-05 08:48:25 -0600

    Update StartGame.java

--- StartGame.java
+++ StartGame.java
@@ -15,6 +15,16 @@ public class StartGame {
     
     // this won't work until you create a getHealth method in your object. Uncomment it when you do.
     // System.out.println(player1.getName() + "'s health is: " + player1.getHealth());
+    
+   
+    Weapon weapon1 = new Weapon();
+    weapon1.setName("M4");
+    weapon1.setDamage(50);
+    System.out.println("Both players are fighting with " + weapon1.getName() + "'s");
+   
+    System.out.println(player1.getName() + "'s health is: " + player1.getHealth() + " percent");
+    System.out.println(player1.getName() + "'s weapon has: " + weapon1.getAmmo() + " bullets");
+    System.out.println(player1.getName() + "'s weapon does: " + weapon1.getDamage() + " damage");
   }  
   
 }
