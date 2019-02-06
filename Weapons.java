059e08118bc427a82545c649bd2692313e79276e
chloehammes <47236158+chloehammes@users.noreply.github.com>
2019-02-06 08:43:37 -0600

    Update Weapons.java

--- Weapons.java
+++ Weapons.java
@@ -4,6 +4,25 @@ public class Weapon {
   private int weaponDamage;
   private int weaponAmmo;
   
+  public static void main(String[] args) {
+  //Creates an array with a max of 5 references
+    Weapon[] weaponArray = new Weapon[5];
+    
+   //Each weapon is created manually
+    Weapon weapon0 = new Weapon();
+    weapon0.setName("M4");
+    
+    weaponArray[0] = weapon0;
+    
+    //As long as i is less than 5 it will print the players from player0 to player4
+    for (int i =0; i< weaponArray.length; i++) {
+      if (weaponArray[i] != null) {
+        System.out,println("Weapon" + i + " name is: " + weaponArray[i].getName());
+        }
+      }
+      
+   }
+  
   //Automatically set new weapons with 35 bullets
   Weapon() {
     this.weaponAmmo = 35;
